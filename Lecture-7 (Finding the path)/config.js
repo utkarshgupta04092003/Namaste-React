3ec37b2461349c54838baa139c2b5cf7923a7fed
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2023-09-25 22:19:24 +0530

    Update config.js
    API_URL modified

--- Lecture-7 (Finding the path)/config.js
+++ Lecture-7 (Finding the path)/config.js
@@ -1,6 +1,6 @@
 export const IMG_CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'; 
 
-export const API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+export const API_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
 
 export const MENU_API_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
